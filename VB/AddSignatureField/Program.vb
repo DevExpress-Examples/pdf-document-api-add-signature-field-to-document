d8658862f371a2c4f15daf4688455f5c54d6c3ca
DevExpressExampleBot <fakeEmail>
2025-12-25 02:29:41 +0400

    autogenerate VB

--- VB/AddSignatureField/Program.vb
+++ VB/AddSignatureField/Program.vb
@@ -1,14 +1,16 @@
 Imports DevExpress.Pdf
+Imports System.Diagnostics
 Imports System.Drawing
 
 Namespace AddSignatureField
 
     Friend Class Program
 
+        <SupportedOSPlatform("windows6.1")>
         Shared Sub Main(ByVal args As String())
             Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Create an empty document. 
-                processor.CreateEmptyDocument("..\..\Result.pdf")
+                processor.CreateEmptyDocument("..\..\..\Result.pdf")
                 ' Create graphics and draw a signature field.
                 Using graphics As PdfGraphics = processor.CreateGraphics()
                     DrawSignatureField(graphics)
@@ -16,13 +18,16 @@ Namespace AddSignatureField
                     processor.RenderNewPage(PdfPaperSize.Letter, graphics)
                 End Using
             End Using
+
+            Process.Start(New ProcessStartInfo("..\..\..\Result.pdf") With {.UseShellExecute = True})
         End Sub
 
+        <SupportedOSPlatform("windows6.1")>
         Private Shared Sub DrawSignatureField(ByVal graphics As PdfGraphics)
             ' Create a signature field specifying its name and location.
             Dim signature As PdfGraphicsAcroFormSignatureField = New PdfGraphicsAcroFormSignatureField("signature", New RectangleF(0, 20, 120, 130))
             ' Specify a content image for the signature field.
-            Dim image As Image = Image.FromFile("..\..\Image.png")
+            Dim image As Image = System.Drawing.Image.FromFile("..\..\..\Image.png")
             signature.ContentImage = image
             ' Add the field to the document.
             graphics.AddFormField(signature)
